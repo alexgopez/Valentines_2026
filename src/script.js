27da2ada468f144373ee5f21808b97271106397a
alexgopez <alexgopez105@gmail.com>
2026-02-13 16:07:58 +0800

    added .nojekyll to fix pathing issues

--- src/script.js
+++ src/script.js
@@ -21,14 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log("Form submitted, name:", name);
       
       const pages = {
-        "althea": "pages/thea.html",
-        "thea": "pages/thea.html",
-        "althea roby ocampo": "pages/thea.html",
-        "fritz": "pages/fritz.html",
-        "fritzhie rein magtoto": "pages/fritz.html",
-        "tonyeng": "pages/tonyeng.html",
-        "pau": "pages/tonyeng.html",
-        "anthony paulo perez" : "pages/tonyeng.html",
+        "althea": "./pages/thea.html",
+        "thea": "./pages/thea.html",
+        "althea roby ocampo": "./pages/thea.html",
+        "fritz": "./pages/fritz.html",
+        "fritzhie rein magtoto": "./pages/fritz.html",
+        "tonyeng": "./pages/tonyeng.html",
+        "pau": "./pages/tonyeng.html",
+        "anthony paulo perez" : "./pages/tonyeng.html",
       };
 
       if (pages[name]) {
